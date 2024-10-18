73bbcf585a6dcde47f5c1424c33e56fa206fc2c5
conancos <15524757+conancos@users.noreply.github.com>
2024-10-18 03:12:53 +0200

    add urls, buttons, footer, styles, logic for navigators and share quotes, svgs, callback function, variables for setProperties and something else

--- src/app.jsx
+++ src/app.jsx
@@ -1,47 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import { RandomColor } from './components/RandomColor'
-import RandomQuote from './components/RandomQuote'
-import './app.css'
+import React, { useState, useEffect } from 'react';
+import { RandomColor } from './components/RandomColor';
+import RandomQuote from './components/RandomQuote';
+import './app.css';
+import X from "./assets/icons/x.svg";
+import In from "./assets/icons/in.svg";
 
 export default function App() {
   const [color, setColor] = useState('crimson');
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
 
-  const handleclick = () => {
+  // función callback que retornará la quote y el author del hijo para pasarlo a la variable tweetUrl desde aquí(el padre)
+  const handleNewQuote = ({ quote, author }) => {
+    setQuote(quote); 
+    setAuthor(author);
+  };
+
+  // función que inicia cambiando el color para desencadenar el cambio de la quote y author.
+  const handleClickAction = () => {
     setColor(RandomColor());
-  }
-  
+  };
+
   useEffect(() => {
-    document.documentElement.style.setProperty('--current-color', color);
-    document.documentElement.style.setProperty('--current-color-shadow', color);
-   
-     (color === "blue" || color === "mediumblue" || color === "darkblue" || color === "midnightblue" || color === "black" || color === "navy" || color === "indigo" || color === "maroon") ?
-      document.documentElement.style.setProperty('--current-bgcolor-dark', 'ivory') :
-      document.documentElement.style.setProperty('--current-bgcolor-dark', '#041c25')
+    const changeValue = (property, value) => {
+      document.documentElement.style.setProperty(property, value);
+    };
+    changeValue('--current-color', color);
+    changeValue('--current-color-shadow', color);
+    if (["blue", "mediumblue", "darkblue", "midnightblue", "black", "navy", "indigo", "maroon"].includes(color)) {
+      changeValue('--current-bgcolor-dark', 'ivory');
+      changeValue('--current-color-icons', "transparent");
+    } else {
+      changeValue('--current-bgcolor-dark', '#041c25');
+      changeValue('--current-color-icons', "var(--current-color)");
+    }
   }, [color]);
   
+
+  // URLs para compartir RandomQuote ("quote and author") en Twitter.
+  const isBrave = navigator.brave || (navigator.userAgent.includes("Brave"));
+  const tweetUrl = isBrave
+  ? `https://twitter.com/intent/post?text=${encodeURIComponent(`#Quotes "${quote}" - ${author}`)}`
+  : `https://twitter.com/intent/tweet?text=${encodeURIComponent(`#Quotes "${quote}" - ${author}`)}`;
+  /* const url = window.location.href; */
+  
+  const referenceURL = "window.location.href"; // "https://conancos.dev/portfolio";
+
+  // URLs para compartir RandomQuote ("quote and author") en LinkedIn.
+  const linkedInUrl= `https://www.linkedin.com/feed/?linkOrigin=LI_BADGE&shareActive=true&shareUrl=${encodeURIComponent(`${referenceURL}`)}&text=${encodeURIComponent(`"${quote}" - ${author}`)}`;
+
 
   return (
     <main>
       <article id="quote-box">
-        <RandomQuote  isNewColor={color} />
-        {/* <section id="quote-and-author" aria-live="polite">
-          <p id="text" className="text-res">{quote}</p>
-          <p id="author" className="author-res">- {author}</p>
-        </section> */}
+        <RandomQuote isNewColor={color} onNewQuote={handleNewQuote} />
         <section id="buttons">
-          {
-            navigator.userAgent.browser !== 'Brave' ?
-            <a id="tweet-quote" className="tweet-quote-res" aria-label="Tweet this quote" target="_blank" rel="noreferrer noopener" href="https://twitter.com/intent/tweet">Tweet</a>
-            :
-            <a id="go-to-twitter" className="go-to-twitter-res" aria-label="Go to Twitter" target="_blank" rel="noreferrer noopener" href="https://twitter.com">tweet on Twitter</a>
-          }
-          <button id="new-quote" onClick={handleclick}>New quote</button>  
+          <aside id="container-rrss">
+            <a id="tweet-quote" className="tweet-quote tweet-quote-res" 
+              aria-label="Tweet this quote" target="_blank" rel="noreferrer noopener" 
+              href={tweetUrl} title="Tweet on X">
+                <img src={X} alt="X" className="icons" />
+            </a>
+            <a id="linkedIn-quote" className="tweet-quote tweet-quote-res" rel="noreferrer noopener"
+              aria-label="Share on LinkedIn" target="_blank" 
+              href={linkedInUrl} title="Share on LinkedIn">
+                <img src={In} alt="In" className="icons" />
+            </a>
+          </aside>
+          <button id="new-quote" onClick={handleClickAction}>New quote</button>  
         </section>
       </article>
+      <footer className="footer">
+        <a href="https://conancos.dev/portfolio" 
+          target="_blank" aria-label="make by..."
+        >by conancos.dev
+        </a>
+      </footer>
     </main>
-  )
-}
-/* 
-href="https://twitter.com/intent/tweet ??
-?hashtags=quotes&related=freecodecamp&text=prueba-de-tweet
-*/
+  );
+
+};
--- src/components/RandomQuote.jsx
+++ src/components/RandomQuote.jsx
@@ -1,33 +1,37 @@
 import { useState, useEffect } from 'react';
 
-const RandomQuote = ({ isNewColor}) => {
-    const [quoteData, setQuoteData] = useState('');
+const RandomQuote = ({ isNewColor, onNewQuote }) => {
+  const [quoteData, setQuoteData] = useState({ quote: '', author: '' });
 
-    useEffect(() => {
-        fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
-            .then(res => res.json())
-            .then(data => {
-                const randomQuote = data.quotes[Math.floor(Math.random() * data.quotes.length)]
-                setQuoteData(randomQuote); 
-                console.log(randomQuote);
-            })
-    }, [isNewColor]);
+  useEffect(() => {
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+      .then(res => res.json())
+      .then(data => {
+        const randomQuote = data.quotes[Math.floor(Math.random() * data.quotes.length)];
+        setQuoteData(randomQuote);
+        console.log(randomQuote);
 
-    const { quote, author } = { ...quoteData };
-        
-    return (
-        <section id="quote-and-author" aria-live="polite">
-            { quote ? ( 
-                <>
-                    <p id="text" className="text-res">{quote}</p>
-                    <p id="author" className="author-res">- {author}</p> 
-                </> 
-                ) : <p id="text" className="text-res">Loading...</p>
-            }
-        </section>    
-    )
-    
+        // llamamos a la función callback onNewQuote para pasarle la quote y el author al padre
+        if (onNewQuote) {
+          onNewQuote(randomQuote);
+        }
+      });
+  }, [isNewColor]);
 
-}
+  const { quote, author } = quoteData;
 
-export default RandomQuote
+  return (
+    <section id="quote-and-author" aria-live="polite" key={quote} >
+      {quote && author ? (
+        <>
+          <p id="text" className="text-res">{quote}</p>
+          <p id="author" className="author-res">- {author}</p>
+        </>
+      ) : (
+        <p id="text" className="text-res">Loading...</p>
+      )}
+    </section>
+  );
+};
+
+export default RandomQuote;
